4c7c62b24a584664a0fec84b493403b083b18bd7
Joey <joseph_buono@uri.edu>
2021-09-26 22:19:37 -0400

    Add and Remove Tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 function App() {
@@ -27,12 +28,29 @@ function App() {
           },
       ]
   )
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
 
   //returning JSX
   return (
     <div className="container">
      <Header/>
-     <Tasks tasks={tasks}/>
+     <AddTask onAdd={addTask}/>
+     {tasks.length > 0 ?
+     (<Tasks tasks={tasks} onDelete={deleteTask}
+     onToggle={toggleReminder}/>)
+     : ('No Tasks To Show')}
     </div>
   );
 }
